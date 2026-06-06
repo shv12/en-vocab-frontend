cd70b128c77119391613a3abae0b873e1b723c8b
Віктор Шилов <vaa_2001@ukr.net>
2026-06-06 15:41:50 +0300

    prepare for github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/en-vocab-frontend/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
